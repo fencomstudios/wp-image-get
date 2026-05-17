84aa675eec6eb2bb8608548423b2464d191b2d05
fencomstudios <fencomstudios@gmail.com>
2026-05-16 19:34:28 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	url := "https://whitherward.com/wp-content/uploads/2017/02/ww_photo_2_dark.jpg"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("request error: %v", string(err))
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Errorf("response error: %v", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Errorf("data read error: %v", err)
+	}
+
+	fmt.Println("Content type is: " + http.DetectContentType(data))
+}
