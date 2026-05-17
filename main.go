659e80ad1bcfbe0f1e7678babe00b8981a1ca8ce
fencomstudios <fencomstudios@gmail.com>
2026-05-16 19:36:26 -0700

    fixed print function calls

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ func main() {
 	url := "https://whitherward.com/wp-content/uploads/2017/02/ww_photo_2_dark.jpg"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("request error: %v", string(err))
+		fmt.Println(fmt.Errorf("request error: %v", err))
 	}
 
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Errorf("response error: %v", err)
+		fmt.Println(fmt.Errorf("response error: %v", err))
 	}
 	defer res.Body.Close()
 
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Errorf("data read error: %v", err)
+		fmt.Println(fmt.Errorf("data read error: %v", err))
 	}
 
 	fmt.Println("Content type is: " + http.DetectContentType(data))
