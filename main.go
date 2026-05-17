657e036c881ef559b0735be5f05c0736fbad75ca
fencomstudios <fencomstudios@gmail.com>
2026-05-17 12:58:11 -0700

    exploring xml tag navigation, test data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
@@ -9,10 +10,55 @@ import (
 	"strings"
 )
 
+type ImageURL struct {
+	ImageURL string `xml:",chardata"`
+}
+
 func main() {
 	url := "https://whitherward.com/wp-content/uploads/2017/02/ww_photo_2_dark.jpg"
 	urlSegments := strings.Split(url, "/")
 	fileName := urlSegments[len(urlSegments) - 1]
+
+	testData := `
+	<rss>
+		<channel>
+			<item>
+				<guid>http://whitherward.com/wp-content/uploads/2017/02/home-1.jpg</guid>
+			</item>
+		</channel>
+	</rss>
+	`
+
+	//xmlFile, err := os.ReadFile("whitherward.WordPress.2026-05-17.xml")
+	//if err != nil {
+	//	fmt.Println(fmt.Errorf("xml file error: %v", err))
+	//}
+
+	//var urlList []ImageURL
+	testDataReader := strings.NewReader(testData)
+	decoder := xml.NewDecoder(testDataReader)
+	
+	//if err := xml.Unmarshal([]byte(testData), &urlList); err != nil {
+	//	fmt.Println(fmt.Errorf("xml file error: %v", err))
+	//}
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			fmt.Println(fmt.Errorf("%v", err))
+			break
+		}
+		if err == io.EOF {
+			fmt.Println(fmt.Errorf("%v"), err)
+			break
+		}
+		
+		if c, ok := token.(xml.StartElement); ok {
+			if c.Name.Local == "guid" {
+				
+			}
+		}
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
