eb7a3ecd88759ad25412b1259c031dae5e971cdb
fencomstudios <fencomstudios@gmail.com>
2026-05-18 21:39:04 -0700

    it works! just need to figure out custom file paths

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io"
@@ -8,57 +10,16 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
-type ImageURL struct {
-	ImageURL string `xml:",chardata"`
-}
+//type ImageURL struct {
+//	ImageURL string `xml:",chardata"`
+//}
 
-func main() {
-	url := "https://whitherward.com/wp-content/uploads/2017/02/ww_photo_2_dark.jpg"
+func getImage(url string) {
 	urlSegments := strings.Split(url, "/")
 	fileName := urlSegments[len(urlSegments) - 1]
-
-	testData := `
-	<rss>
-		<channel>
-			<item>
-				<guid>http://whitherward.com/wp-content/uploads/2017/02/home-1.jpg</guid>
-			</item>
-		</channel>
-	</rss>
-	`
-
-	//xmlFile, err := os.ReadFile("whitherward.WordPress.2026-05-17.xml")
-	//if err != nil {
-	//	fmt.Println(fmt.Errorf("xml file error: %v", err))
-	//}
-
-	//var urlList []ImageURL
-	testDataReader := strings.NewReader(testData)
-	decoder := xml.NewDecoder(testDataReader)
-	
-	//if err := xml.Unmarshal([]byte(testData), &urlList); err != nil {
-	//	fmt.Println(fmt.Errorf("xml file error: %v", err))
-	//}
-
-	for {
-		token, err := decoder.Token()
-		if err != nil {
-			fmt.Println(fmt.Errorf("%v", err))
-			break
-		}
-		if err == io.EOF {
-			fmt.Println(fmt.Errorf("%v"), err)
-			break
-		}
-		
-		if c, ok := token.(xml.StartElement); ok {
-			if c.Name.Local == "guid" {
-				
-			}
-		}
-	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -82,5 +43,60 @@ func main() {
 	fileErr := os.WriteFile("./downloads/" + fileName, data, 0666)
 	if fileErr != nil {
 		log.Fatal(err)
+	}
+
+	time.Sleep(3 * time.Second)
+}
+
+func getFilename() (string) {
+	for {
+		fmt.Print("Please enter the filename of the Wordpress Image Export XML: ")
+		inputReader := bufio.NewReader(os.Stdin)
+		inputString, err := inputReader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("file path error: %v\n", err)
+		} else {
+			return inputString
+		}
+	}
+}
+
+func main() {
+
+	//xmlFile, err := os.ReadFile(getFilename())
+	xmlFile, err := os.ReadFile("whitherward.WordPress.2026-05-17.xml")
+	if err != nil {
+		fmt.Println(fmt.Errorf("xml file error: %v", err))
+	}
+
+
+	xmlBuffer := bytes.NewBuffer(xmlFile)
+	decoder := xml.NewDecoder(xmlBuffer)
+
+	foundGUID := false
+
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			break
+		}
+		if err == io.EOF {
+			break
+		}
+		
+		switch t := token.(type) {
+		case xml.StartElement:
+			if t.Name.Local == "guid" {
+				foundGUID = true
+				fmt.Printf("found a guid!\n")
+			}
+		case xml.CharData:
+			if foundGUID == true {
+				address := strings.TrimSpace(string(t))
+				fmt.Printf("Image address: %v\n", address)
+				getImage(address)
+				foundGUID = false
+			}
+		}
 	}
 }
